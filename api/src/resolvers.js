767793ed53cc2fa8f4eabb565fab6924a0e3011e
hawei <hawei>
2021-04-17 18:05:13 +0800

    Exercise1: * Define a query Type in your Schema(定义实体) * Define fields on your query type（定义查询语句） * run your apllo server ========== * 定义实体 * 定义查询语句，并且创建对应的resolvers * 创建一个servers run 这些你新建的query 语句

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,17 +5,30 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    // _
+    // __: from client query arguments
+    pets(_, __, ctx) {
+      return ctx.models.Pet.findMany();
+      // return [{}, {}];
+    },
+    getUsers(_, __, ctx) {
+      return ctx.models.User.findOne();
     }
+    
+  },
+  // Mutation: {
+    
+  // },
+  Pet: {
+    // id(pet) {
+    //   console.log(pet);
+    //   return 3;
+    // },
+    // img(pet) {
+    //   return pet.type === 'DOG'
+    //     ? 'https://placedog.net/300/300'
+    //     : 'http://placekitten.com/300/300'
+    // }
   },
   User: {
     
