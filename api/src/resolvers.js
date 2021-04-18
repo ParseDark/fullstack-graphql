beacf688205eec964a9895d47f5c2dfed6892f32
hawei <hawei>
2021-04-18 16:10:15 +0800

    Relationship Exercise

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,6 +5,7 @@
 
 module.exports = {
   Query: {
+    // top level resolver
     // _
     // __: from client query arguments define from schema
     // 参数的来源有两种：内置类型，输入类型：
@@ -32,16 +33,25 @@ module.exports = {
   },
   Pet: {
     // id(pet) {
-    //   console.log(pet);
     //   return 3;
     // },
     img(pet) {
       return pet.type === 'DOG'
         ? 'https://placedog.net/300/300'
         : 'http://placekitten.com/300/300'
+    },
+    // field level resolver
+    // 字段级别的resolver 首个参数是父级的data
+    // 第二个参数是input 参数
+    // 第三个参数是context
+    owner(pet, __, ctx) {
+      console.log(ctx.models.User.findOne());
+      return ctx.models.User.findOne();
     }
   },
   User: {
-    
+    pets(user, __, ctx) {
+      return ctx.models.Pet.findMany();
+    }
   }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -7,6 +7,7 @@ const typeDefs = gql`
     type User {
         id: ID!
         username: String!
+        pets: [Pet]
     }
 
     type Pet {
@@ -15,6 +16,7 @@ const typeDefs = gql`
         name: String!
         type: String!
         img: String!
+        owner: User!
     }
 
     # 输入类型：通过input关键字定义输入字段的shape 
